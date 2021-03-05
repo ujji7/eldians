b01cb563536c7dd7255f65cdd42eaca2920a011d
DanielleCreary-Thomas <daniellewc.thomas@mail.utoronto.ca>
2021-03-05 18:52:59 -0500

    type sigs added

---
+++ AbstractUser.java
@@ -0,0 +1,39 @@
+/**
+ * Abstract class for User objects.
+ */
+public class AbstractUser {
+    public String username;
+    public float accountBalance;
+    public Arraylist inventory;
+
+    public AbstractUser(String username){
+        this.username = username;
+        this.accountBalance = 0.00;
+        this.inventory = new Arraylist<>();
+
+    }
+
+    public void addCredit(){
+
+    }
+
+    public void buy(){
+
+    }
+
+    public void sell(){
+
+    }
+
+    public void refund(){
+
+    }
+
+    public void create(){
+
+    }
+
+    public void delete{
+        
+    }
+}
