5c09f15b211105a7be0dd5a36d7647ef9b24539a
Bharathi Srinivasan <bhara280@gmail.com>
2021-03-25 19:45:47 -0400

    updated constructors

--- src/main/BuyUser.java
+++ src/main/BuyUser.java
@@ -5,9 +5,18 @@ package main;
  */
 public class BuyUser extends AbstractUser {
 
-    public BuyUser(String username, Float credit) {
+    public BuyUser(String username) {
+        super(username);
+    }
+
+    public BuyUser(String username, Double credit) {
         super(username);
         this.accountBalance = credit;
+    }
+
+    public BuyUser(String username, Double balance, ArrayList<Game> inventory, ArrayList<String> transactions) {
+        super(username, balance, inventory, transactions);
+//        this.transactionHistory = transactions;
     }
 
     /** Prints that the user cannot sell a game.
