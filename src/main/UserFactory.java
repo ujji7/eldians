f4eef85ef4757f960ad8ae8102f275a0d341fbb3
Bharathi Srinivasan <bhara280@gmail.com>
2021-03-24 20:43:07 -0400

    completed factory and added test files for readin json

--- src/main/UserFactory.java
+++ src/main/UserFactory.java
@@ -1,19 +1,25 @@
 package main;
 
-public class UserFactory {
+import java.util.ArrayList;
 
-    public AbstractUser makeUser(String type) {
+public class UserFactory {
+    private final String admin = "AA";
+    private final String buy = "BS";
+    private final String sell = "SS";
+    private final String full = "FS";
+
+    public AbstractUser makeUser(String name, String type, double balance, ArrayList<Game> inventory, ArrayList<String> transactions) {
 
         AbstractUser user = null;
 
-        if (type.equals("Admin")) {
-            user = new AdminUser();
-        } else if (type.equals("Sell")) {
-            user = new Sell();
-        } else if (type.equals("Buy")) {
-            user = new Buy();
-        } else if (type.equals("Full")) {
-            user = new FullStandardUser();
+        if (type.equals(admin)) {
+            user = new AdminUser(name, balance, inventory, transactions);
+        } else if (type.equals(sell)) {
+            user = new SellUser(name, balance, inventory, transactions);
+        } else if (type.equals(buy)) {
+            user = new BuyUser(name, balance, inventory, transactions);
+        } else if (type.equals(full)) {
+            user = new FullStandardUser(name, balance, inventory, transactions);
         }
 
         return user;
