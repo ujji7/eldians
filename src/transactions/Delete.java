a320cb2b9aaff6b59721d83029f8814723aa2792
m4deo <madeo.arturi@mail.utoronto.ca>
2021-03-29 13:40:57 -0400

    Completed all Execute functions

--- src/transactions/Delete.java
+++ src/transactions/Delete.java
@@ -10,16 +10,50 @@ public class Delete implements Transaction {
 
     String username;
     String type;
-    float funds;
+    double funds;
 
+    /**
+     * Creates a new Delete transaction.
+     *
+     * @param u String representing the username.
+     * @param t String representing the user type.
+     * @param f String representing the funds.
+     */
     public Delete(String u, String t, String f) {
         this.username = u;
         this.type = t;
-        this.funds = Float.parseFloat(f);
+        this.funds = Double.parseDouble(f);
     }
 
+    /**
+     * Executes a Delete transaction. Returns the user currently logged in.
+     *
+     * @param users ArrayList of Users in the system.
+     * @param games ArrayList of Games in the system.
+     * @param market the current Marketplace, holding games being sold by sellers.
+     * @param login the user who is currently logged in.
+     * @return the user currently logged in.
+     */
     @Override
-    public String execute(ArrayList<AbstractUser> users, ArrayList<Game> games, Marketplace market, String login) {
-        return null;
+    public AbstractUser execute(ArrayList<AbstractUser> users, ArrayList<Game> games, Marketplace market,
+                                AbstractUser login) {
+
+        // Find the user being deleted
+        AbstractUser delete = null;
+        for (AbstractUser user : users) {
+            if (user.getUsername().equals(this.username)) {
+                delete = user;
+            }
+        }
+
+        // Delete if they exist, raise error otherwise
+        if (delete == null) {
+            System.out.println("ERROR: < Cannot delete " + this.username + " as the user does not exist in the " +
+                    "system > ");
+        } else {
+            login.delete(delete, this.funds);
+        }
+
+        return login;
     }
 }
--- src/transactions/Gift.java
+++ src/transactions/Gift.java
@@ -25,8 +25,54 @@ public class Gift implements Transaction {
         this.receiverName = r;
     }
 
+    /**
+     * Executes Gift transaction. Return the user currently logged in.
+     *
+     * @param users ArrayList of Users in the system.
+     * @param games ArrayList of Games in the system.
+     * @param market the current Marketplace, holding games being sold by sellers.
+     * @param login the user who is currently logged in.
+     * @return the user currently logged in.
+     */
     @Override
-    public String execute(ArrayList<AbstractUser> users, ArrayList<Game> games, Marketplace market, String login) {
-        return null;
+    public AbstractUser execute(ArrayList<AbstractUser> users, ArrayList<Game> games, Marketplace market,
+                                AbstractUser login) {
+
+        // Find game
+        Game game = null;
+        for (Game g: games) {
+            if (g.getName().equals(this.gameName)) {
+                game = g;
+            }
+        }
+
+        // Find owner
+        AbstractUser owner = null;
+        for (AbstractUser user: users) {
+            if (user.getUsername().equals(this.ownerName)) {
+                owner = user;
+            }
+        }
+
+        // Find receiver
+        AbstractUser receiver = null;
+        for (AbstractUser user: users) {
+            if (user.getUsername().equals(this.receiverName)) {
+                receiver = user;
+            }
+        }
+
+        // If game, owner, and receiver all exist, execute AbstractUser.gift, raise error otherwise
+        if (game == null) {
+            System.out.println("ERROR: < Cannot find " + this.gameName + " in the system. > ");
+        } else if (owner == null) {
+            System.out.println("ERROR: < Cannot find " + this.ownerName + " in the system. > ");
+        } else if (receiver == null) {
+            System.out.println("ERROR: < Cannot find " + this.receiverName + " in the system. > ");
+        } else {
+            login.gift(owner, receiver, game);
+        }
+
+        return login;
     }
 }
