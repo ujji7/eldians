f76b9ee26e12ba7416a5ce8dac16bd39ee744bef
Bharathi Srinivasan <bhara280@gmail.com>
2021-04-02 20:59:08 -0400

    Wrote game reading test cases

--- src/test/DatabaseReadTest.java
+++ src/test/DatabaseReadTest.java
@@ -13,10 +13,11 @@ import java.io.PrintStream;
 import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 //THIS TEST IS TO CHECK THAT DATABASE READS INFO CORRECTLY
 //HAVE DIFF TEST FILES - ONE FOR ALL THE WEIRD AND EDGE CASE
@@ -32,20 +33,33 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class DatabaseReadTest {
 
     File Game, User, Market;
-    String filePath;
-    String fileNameGameErrors, fileNameGameNonExistent, fileNameGameEmpty, fileNameGameFormat, fileNameGameOneGame,
-    fileNameGameGood;//games file
-    String fileNameUserErrors, fileNameUserNonExistent, fileNameUserEmpty, fileNameUserFormat, fileNameUserOneUser;
-    String fileNameMarketErrors, fileNameMarketNonExistent, fileNameMarketEmpty, fileNameMarketFormat,
-            fileNameMarketNoAuction, fileNameMarketAuctionWrong, fileNameMarketNoUID;
-
+    String filePath = "DatabaseTestFiles/";
+    String fileNameGameErrors = filePath + "GameErrors.json";
+    String fileNameGameNonExistent = filePath + "GameNonExistent.json";
+    String fileNameGameEmpty = filePath + "GameEmpty.json";
+    String fileNameGameFormat = filePath + "GameFormat.json";
+    String fileNameGameOneGame = filePath + "GameOneGame.json";
+    String fileNameGameGood = filePath + "GameGood.json";
+    String fileNameUserErrors = filePath + "UserErrors.json";
+    String fileNameUserNonExistent = filePath + "UserNonExistent.json";
+    String fileNameUserEmpty = filePath + "UserEmpty.json";
+    String fileNameUserFormat = filePath + "UserFormat.json";
+    String fileNameUserOneUser = filePath + "UserOneUser.json";
+    String fileNameUserGood = filePath + "UserGood.json";
+    String fileNameMarketErrors = filePath + "MarketErrors.json";
+    String fileNameMarketNonExistent = filePath + "MarketNonExistent.json";
+    String fileNameMarketEmpty = filePath + "MarketEmpty.json";
+    String fileNameMarketFormat = filePath + "MarketFormat.json";
+    String fileNameMarketNoAuction = filePath + "MarketNoAuction.json";
+    String fileNameMarketAuctionWrong = filePath + "MarketAuctionWrong.json";
+    String fileNameMarketNoUID = filePath + "MarketNoUID.json";
+    String fileNameMarketGood = filePath + "MarketGood.json";
     String fileNotFoundError = " file not found. An empty ";
     String fileFormatError = " file not in correct format. An empty ";
     String fileErrorEnd = " will be created.\r\n";
-
     ArrayList<Game> emptyGames;
     ArrayList<AbstractUser> emptyUsers;
-    
+    List<Game> games;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
 
@@ -53,25 +67,8 @@ public class DatabaseReadTest {
     @BeforeEach
     public void setUp() {
         System.setOut(new PrintStream(outContent));
-        filePath = "DatabaseTestFiles/";
-        fileNameGameErrors = filePath + "GameErrors.json";
-        fileNameGameNonExistent = filePath + "GameNonExistent.json";
-        fileNameGameEmpty = filePath + "GameEmpty.json";
-        fileNameGameFormat = filePath + "GameFormat.json";
-        fileNameGameOneGame = filePath + "GameOneGame.json";
-        fileNameGameGood = filePath + "GameGood.json";
-        fileNameUserErrors = filePath + "UserErrors.json";
-        fileNameUserNonExistent = filePath + "UserNonExistent.json";
-        fileNameUserEmpty = filePath + "UserEmpty.json";
-        fileNameUserFormat = filePath + "UserFormat.json";
-        fileNameUserOneUser = filePath + "UserOneUser.json";
-        fileNameMarketErrors = filePath + "MarketErrors.json";
-        fileNameMarketNonExistent = filePath + "MarketNonExistent.json";
-        fileNameMarketEmpty = filePath + "MarketEmpty.json";
-        fileNameMarketFormat = filePath + "MarketFormat.json";
-        fileNameMarketNoAuction = filePath + "MarketNoAuction.json";
-        fileNameMarketAuctionWrong = filePath + "MarketAuctionWrong.json";
-        fileNameMarketNoUID = filePath + "MarketNoUID.json";
+        ReadingJSON.setGameFileName(fileNameGameErrors);
+        games = ReadingJSON.readGamesFile();
         emptyGames = new ArrayList<Game>();
         emptyUsers = new ArrayList<AbstractUser>();
     }
@@ -81,6 +78,26 @@ public class DatabaseReadTest {
         System.setOut(originalOut);
     }
 
+    
+    public boolean equalArraylist(ArrayList<Integer> search, ArrayList<Integer> all) {
+        
+        for (Integer i : search) {
+            if (!all.contains(i)) {
+                System.out.println(i);
+                return false;
+            }
+        }
+
+        for (Integer j : all) {
+            if (!search.contains(j)) {
+                System.out.println(j);
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
     @Test
     public void testGameNonExistent() {
         ReadingJSON.setGameFileName(fileNameGameNonExistent);
@@ -111,6 +128,28 @@ public class DatabaseReadTest {
         List<Game> games = ReadingJSON.readGamesFile();
         assertEquals("Games" + fileFormatError + "list of games" + fileErrorEnd, outContent.toString());
         assertEquals(emptyGames, games);
+    }
+
+    @Test
+    public void testGameErrors1() {
+        for (Game g : games) {
+            System.out.println(g.getUniqueID());
+        }
+        assertEquals( 7, games.size());
+    }
+
+    @Test
+    public void testGameErrors2() {
+        ArrayList<Integer> allIds = new ArrayList<Integer>();
+        for (Game g : games) {
+            allIds.add(g.getUniqueID());
+        }
+        ArrayList<Integer> search = new ArrayList<>(Arrays.asList(1, 2, 5, 6, 7, 12, 13));
+        assertTrue(allIds.containsAll(search));
+        System.out.println(equalArraylist(search, allIds));
+//        assertTrue(equalArraylist(search, allIds));
+        System.out.println(outContent.toString());
+        assertTrue(equalArraylist(search, allIds));
     }
 
     @Test
@@ -152,6 +191,5 @@ public class DatabaseReadTest {
         assertEquals("Users" + fileFormatError + "list of users" + fileErrorEnd, outContent.toString());
         assertEquals(emptyUsers, users);
     }
-    
     
 }
